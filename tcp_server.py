21a09d5d70f719a56f8df9d831869b78cf158ee6
xGp35 <girijaprasanna1999@gmail.com>
2026-05-01 01:25:16 +0530

    added back response code

--- tcp_server.py
+++ tcp_server.py
@@ -7,6 +7,8 @@ SERVER_PORT = 9000
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Add some optionsal settings to change the default behaviour of sockets
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.setblocking(True)
+
 
 server_socket.bind((SERVER_HOST, SERVER_PORT))
 
@@ -23,40 +25,42 @@ response = (
     "\r\n"
     f"{response_body}"
 )
-while True:
-    try:
-        client_socket, client_address = server_socket.accept()
-        print(f"Client Socket: {client_socket}")
-        print(f"Connected by: {client_address}")
-    except:
-        time.sleep(1)
-        continue    
-
-
 # while True:
-#     print("Waiting for client...")
 #     try:
 #         client_socket, client_address = server_socket.accept()
-#         # accept() says: “Pause here until someone connects.”
-#         # accepts any TCP connection, not specifically HTTP.
-#         # browsers communicate using the HTTP protocol over TCP, Brave connected and sent an HTTP request.
-
 #         print(f"Client Socket: {client_socket}")
 #         print(f"Connected by: {client_address}")
-#         # Till this much I am just starting a TCP server.
-
-#         data = client_socket.recv(1024)
-#         # recv() means: “Wait and read incoming data from the client.”
-#         # Receive up to 1024 bytes of data from the connected client.
-#         print("Decoding data...")
-#         print(data.decode())
-
-#         client_socket.send(response.encode())
-
-#         client_socket.close()
 #     except:
-#         time.sleep(2)
-#         continue
+#         time.sleep(1)
+#         print("I am in except block")
+#         continue    
+
+
+while True:
+    print("Waiting for client...")
+    try:
+        client_socket, client_address = server_socket.accept()
+        # accept() says: “Pause here until someone connects.”
+        # accepts any TCP connection, not specifically HTTP.
+        # but if conection is set to non blocking, then this line will throw an error if there are no connection in queue.
+        # browsers communicate using the HTTP protocol over TCP, Brave connected and sent an HTTP request.
+
+        print(f"Client Socket: {client_socket}")
+        print(f"Connected by: {client_address}")
+        # Till this much I am just starting a TCP server.
+
+        data = client_socket.recv(1024)
+        # recv() means: “Wait and read incoming data from the client.”
+        # Receive up to 1024 bytes of data from the connected client.
+        print("Decoding data...")
+        print(data.decode())
+
+        client_socket.send(response.encode())
+
+        client_socket.close()
+    except:
+        time.sleep(2)
+        continue
 
 
 # Some clients are lenient, but official HTTP specification expects:
